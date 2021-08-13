3f5a81d23ea7628efc50104b2b9fcb98766b7793
Nycole Xavier <74930052+nycolexavier@users.noreply.github.com>
2021-08-13 10:48:44 -0300

    add token

--- bot.js
+++ bot.js
@@ -2,8 +2,11 @@ console.log('Beep beep! 🤖');
 
 require('dotenv').config();
 
+// importa o módulo discord.js
 const Discord = require('discord.js');
 const client = new Discord.Client();
+
+//ligação com a API do discord.js e o bot
 client.login(process.env.BOTTOKEN);
 
 client.on("ready", readyDiscord);
